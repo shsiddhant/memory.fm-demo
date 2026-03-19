5c10bb1b432552c3599b8ff44affc7e0af4d8cf5
Siddhant Sharma <sharmasiddhant@protonmail.com>
2026-03-20 00:18:21 +0530

    feat: add home page explaining the project.
    - add links to the repo and docs in the sidebar.
    
    - pin streamlit version to 1.52.2
    
    - remove static fonts serving

--- pages/home.py
+++ pages/home.py
@@ -1,63 +1,6 @@
 from __future__ import annotations
 import streamlit as st
-import json
-from memoryfm.cli import imports_dir
-
-from memoryfm.cli.utils._import_utils import get_imported_names
-from memoryfm.streamlit.util import set_session_data
-from memoryfm.streamlit.manage_imports import add_user, confirm_delete, sync_with_api
-from memoryfm.streamlit.index import overview
-
-if "imports" not in st.session_state or st.session_state.imports is None:
-    st.session_state["imports"] = get_imported_names()
-    st.session_state.imports.sort()
-
-if "imports_source" not in st.session_state:
-    st.session_state["imports_source"] = dict.fromkeys(st.session_state["imports"])
-for username in st.session_state["imports"]:
-    with open(imports_dir / username / f"{username}-meta.json", "r") as fp:
-        st.session_state["imports_source"][username] = json.load(fp)["source"]
-
-
-def users_list():
-    st.session_state.imports.sort()
-    for username in st.session_state.imports:
-        with st.container():
-            user, modify = st.columns([1, 1])
-            with modify:
-                sync, delete = st.columns([1, 5])
-        with user:
-            if st.button(
-                f"**:material/person: {username}**",
-                help="Load user",
-            ):
-                with st.spinner(
-                    text="Loading your data...",
-                ):
-                    set_session_data(username)
-                    st.switch_page(overview)
-        with sync:
-            sync_scrobbles = False
-            if st.session_state["imports_source"][username] == "last.fm":
-                if st.button(
-                    "**:green[:material/sync:]**",
-                    key=f"sync {username}",
-                    type="tertiary",
-                    help="Sync scrobbles",
-                ):
-                    sync_scrobbles = True
-        with delete:
-            if st.button(
-                ":primary[:material/delete:]",
-                key=f"delete {username}",
-                type="tertiary",
-                help="Delete user",
-            ):
-                confirm_delete(username)
-        if st.session_state.deleted_user:
-            st.session_state.deleted_user = None
-        if sync_scrobbles:
-            sync_with_api(username)
+from memoryfm.streamlit.util import PADDING
 
 
 st.markdown(
@@ -74,23 +17,26 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-# Header
-st.title(":red[memory.fm]")
+
+st.title(":primary[memory.fm]")
 "##### *music meets memory*"
 "---"
 
-userslist, adduser = st.columns(2)
-# Users list
-with userslist:
-    st.header(":material/library_music: Your Music")
-    st.markdown("### :material/groups: Users")
-    if st.session_state.imports:
-        "\n"
-        users_list()
-    else:
-        st.write("**No users found**")
-# Add new user
-with adduser:
-    st.header(":material/person_add: New User", help="Add new user")
-    st.write("### :material/download: Import from Last.fm/lastfmstats/Spotify")
-    add_user()
+
+st.markdown("""
+**memory.fm** is a tool for exploring your music listening history from Last.fm and Spotify.
+
+Instead of focusing only on aggregate stats, it surfaces long-term and local patterns such as attachment, repetition, 
+and obsessive listening, to help you revisit periods of your life through music.
+
+*✨ :grey[Inspired by the idea of using music as a way to revisit memories] ✨*
+""")
+
+st.markdown("""
+### Stats and Visuals
+[**:material/list_alt: Overview**](overview) -- A summary of listening history.  
+[**:material/bar_chart: Top Charts**](top_charts) -- The top artists, albums, and tracks by period.  
+[**:material/person_heart: Attachment Index**](attachment) -- How concentrated your listening was on particular artists, albums, or tracks.  
+[**:material/bolt: Streaks**](streaks) -- The periods of obsessive listening.  
+""")
+
